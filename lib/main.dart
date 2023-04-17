c988cb6e5989e36a2fa9d1b536be63c409e000d2
Pavel Kryl <pavel@kryl.eu>
2023-04-17 09:31:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,42 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
+final appState = MyAppState();
 
 void main() {
   runApp(MyApp());
+}
+
+class MyAppState {
+  var current = WordPair.random();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
+  Widget build(BuildContext context) =>
+      MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
-      ),
-    );
-  }
+      );
+
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
 
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
+  Widget build(BuildContext context) =>
+    Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A double random idea:'),
           Text(appState.current.asLowerCase),
         ],
       ),
     );
-  }
 }
